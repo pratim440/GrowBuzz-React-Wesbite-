d2bf3cee23134b6a16887539a0f82de6194e4e67
pratim440 <c.pratim440@gmail.com>
2020-10-06 13:01:12 +0530

    Updates

---
+++ precache-manifest.9ea296da1dd407c83c7da4f088fa3612.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3476b525fd8e99f24e29c38d93dc52",
+    "url": "/GrowBuzz-React-Wesbite-/index.html"
+  },
+  {
+    "revision": "6cab3f7404d16d285de7",
+    "url": "/GrowBuzz-React-Wesbite-/static/css/main.08ebcbe7.chunk.css"
+  },
+  {
+    "revision": "73327b78a323a5fc7d56",
+    "url": "/GrowBuzz-React-Wesbite-/static/js/2.e4a3c5e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GrowBuzz-React-Wesbite-/static/js/2.e4a3c5e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cab3f7404d16d285de7",
+    "url": "/GrowBuzz-React-Wesbite-/static/js/main.d88460bd.chunk.js"
+  },
+  {
+    "revision": "f1ab239f955fab714aba",
+    "url": "/GrowBuzz-React-Wesbite-/static/js/runtime-main.f8677b2a.js"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/about_img.619f0cc8.png"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/home_img.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/software.43049cae.jpeg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/GrowBuzz-React-Wesbite-/static/media/web.1192a86d.jpg"
+  }
+]);
